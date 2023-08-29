b5561b29b2b5586457e678bcf26ff747ddf411cd
aashif743 <mhdaashif743@gmail.com>
2023-08-29 15:33:42 +0530

    Update com

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,26 +7,33 @@ import CanvasLoader from "../Loader";
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.glb");
 
-  return (
-    <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <pointLight intensity={1} />
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
-  );
+
+// Check if the computer model contains a valid scene
+if (!computer.scene) {
+  return null; // Return null if the scene is not valid
+}
+
+const position = computer.scene.position;
+
+// Validate the "position" attribute to ensure it's not NaN
+if (!position || isNaN(position.x) || isNaN(position.y) || isNaN(position.z)) {
+  return null; // Return null if the position attributes are invalid
+}
+
+
+
+return (
+  <mesh>
+    <hemisphereLight intensity={0.30} groundColor='black' />
+    {/* Your lighting and shadow code */}
+    <primitive
+      object={computer.scene}
+      scale={isMobile ? 0.7 : 0.75}
+      position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+      rotation={[-0.01, -0.2, -0.1]}
+    />
+  </mesh>
+);
 };
 
 const ComputersCanvas = () => {
